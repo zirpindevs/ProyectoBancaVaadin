2b120796f5f87a2e5967fb5cf4ea0d8b42652698
zirpin <zirpin@gmail.com>
2021-05-27 20:29:59 +0200

    cuentas view pero no operativo

--- src/main/java/com/example/application/views/cuentas/CuentasView.java
+++ src/main/java/com/example/application/views/cuentas/CuentasView.java
@@ -1,30 +1,183 @@
 package com.example.application.views.cuentas;
 
+import com.example.application.backend.model.*;
+import com.example.application.backend.model.bankaccount.operations.BankAccountUserResponse;
+import com.example.application.backend.model.transaction.operations.TransactionsCreditcardResponse;
+import com.example.application.backend.service.BankAccountService;
+import com.example.application.backend.service.CreditCardService;
+import com.example.application.backend.service.TransactionService;
+import com.example.application.backend.service.UserService;
+import com.example.application.views.tarjetas.form.CreditCardForm;
+import com.github.appreciated.card.Card;
+import com.github.appreciated.card.action.ActionButton;
+import com.github.appreciated.card.action.Actions;
+import com.github.appreciated.card.content.IconItem;
+import com.github.appreciated.card.label.PrimaryLabel;
+import com.github.appreciated.card.label.SecondaryLabel;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import com.example.application.views.main.MainView;
 import com.vaadin.flow.router.RouteAlias;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Route(value = "cuentas", layout = MainView.class)
 @PageTitle("Cuentas")
 public class CuentasView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
 
-    public CuentasView() {
-        addClassName("cuentas-view");
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        add(name, sayHello);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final int NOTIFICATION_DEFAULT_DURATION = 5000;
+
+    private UserService userService;
+    private BankAccountService bankAccountService;
+    private TransactionService transactionService;
+    private TransactionsCreditcardResponse transactionsCreditcardResponse;
+    Map<String, String> map1 = new HashMap<>();
+
+    private List<BankAccount> bankAccounts;
+    private List<BankAccountUserResponse> miListaBanco = null;
+
+
+
+    User miUser = new User();
+    private Binder<CreditCard> creditCardBinder = new BeanValidationBinder<CreditCard>(CreditCard.class);;
+
+
+    public CuentasView(UserService userService, BankAccountService bankaccountService, TransactionService transactionService) {
+  /*      this.setSizeFull();
+        this.setPadding(true);*/
+
+        this.userService = userService;
+        this.bankAccountService = bankaccountService;
+        this.transactionService = transactionService;
+
+        loadData();
+
+
+        //pinta cada bankaccount que tenga el usuario
+        System.out.println(miListaBanco);
+/*
+        miListaBanco.forEach(bankAccount -> add(cardGenerator(bankAccount.getBankAccounts(), bankAccount.getStatus().toString(), bankAccount)));
+*/
+    }
+
+    private void loadData() {
+
+        try {
+
+            miUser = userService.findOne(1L).get();
+
+
+/*
+            this.bankAccounts = (List<BankAccount>) bankAccountService.findAllBankAccountsByIdUser(miUser.getId());
+*/
+
+     /*       if(!creditCards.isEmpty()){
+                System.out.println(creditCards);
+                creditCards.forEach(creditCard -> {
+                    System.out.println(creditCard.getId());
+                    miLista.add(transactionService.findAllTransactionsByDateRangeByIdCreditcard(creditCard.getId(), map1));
+                });
+                System.out.println(miLista);
+            }*/
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+
+            logger.debug(ex.getLocalizedMessage());
+        }
+    }
+
+
+    public Component cardGenerator(Long idCreditCard, String numBankAccount, String status, BankAccount bankAccount) {
+
+/*
+        String maskedNumbers = maskCardNumber(numCreditCard);
+*/
+        HorizontalLayout cardLayout = new HorizontalLayout();
+
+        Image logoBanco = new Image();
+
+        logoBanco = new Image("images/unicaja-logo.png","");
+        logoBanco.setHeight("75px");
+        logoBanco.setWidth("115px");
+
+        map1.put("startDate", "2020-01-01 00:00:00.000000");
+        map1.put("endDate", "2022-05-31 23:59:59.999999");
+        map1.put("page", "0");
+        map1.put("limit", "50");
+
+       /* TransactionsCreditcardResponse balanceTarjeta = transactionService.findAllTransactionsByDateRangeByIdCreditcard(idCreditCard, map1);
+
+        List<TransactionDTO> transactionsTarjetas = balanceTarjeta.getTransactions();
+
+        Double saldoTarjeta = 0D;
+
+        for(int x=0; x<transactionsTarjetas.size();x++) {
+
+            if(transactionsTarjetas.get(x).getTipoMovimiento().name().equals("PAGO") || transactionsTarjetas.get(x).getTipoMovimiento().name().equals("RECIBO"))
+                saldoTarjeta = saldoTarjeta + transactionsTarjetas.get(x).getImporte();
+        }
+*/
+
+
+
+        Card card = new Card(
+                // if you don't want the title to wrap you can set the whitespace = nowrap
+                logoBanco,
+/*
+                new PrimaryLabel(bankAccount.getBalance().toString()+" €"),
+*/
+                new SecondaryLabel(numBankAccount),
+/*
+                new IconItem(getIcon(cardProvider), ""),
+*/
+                new Actions(
+                        new ActionButton("Ver detalles", event -> {
+
+/*
+                            CreditCardForm creditCardForm = new CreditCardForm(creditCard);
+*/
+
+
+                            // open form dialog view
+/*
+                            creditCardForm.open();
+*/
+                        })
+                )
+        );
+        cardLayout.add(card);
+        cardLayout.setAlignItems(Alignment.CENTER);
+        return cardLayout;
+    }
+
+    private Image getIcon(String cardProvider) {
+        Image img = new Image();
+
+        if(cardProvider.equals("Visa"))
+            img = new Image("images/visa.png","");
+        if(cardProvider.equals("MasterCard"))
+            img = new Image("images/mastercard.png","");
+
+        return img;
+    }
+
+    private String maskCardNumber(String cardNumber){
+        return "**** " + cardNumber.substring(cardNumber.length() - 4);
     }
 
 }
---
+++ src/main/java/com/example/application/views/cuentas/form/AccountForm.java
@@ -0,0 +1,97 @@
+package com.example.application.views.cuentas.form;
+
+import com.example.application.backend.model.CreditCard;
+import com.example.application.views.main.MainView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "account", layout = MainView.class)
+@PageTitle("Account")
+public class AccountForm extends Dialog {
+
+    private CreditCard creditCard;
+
+    private TextField  cardNumber = new TextField("Numbero de Tarjeta");
+    private TextField cardholderName = new TextField("Name de la tarjeta");
+    private TextField creditCardType = new TextField("Tipo de tarjeta");
+    private TextField card_provider = new TextField("Proovedor de Tarjeta");
+
+    private Checkbox enabled = new Checkbox();
+
+    private TextField dateField = new TextField("Fecha de Expiracion");
+
+    private PasswordField pin = new PasswordField("PIN");
+    private PasswordField cvv = new PasswordField("CVV");
+
+    private Button Salir = new Button("Salir");
+
+    public AccountForm(CreditCard creditCard) {
+
+        super();
+        this.creditCard = creditCard;
+
+
+        add(createTitle());
+        add(createFormLayout());
+        add(createButtonLayout(creditCard));
+
+        Salir.addClickListener(e -> {
+            close();
+
+        });
+    }
+
+    private Component createTitle() {
+        return new H3("Detalles de la Tarjeta de Credito");
+    }
+
+    private Component createFormLayout() {
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(cardNumber, cardholderName, dateField, creditCardType, card_provider, pin, cvv);
+        return formLayout;
+    }
+
+    private Component createButtonLayout(CreditCard creditCard) {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.addClassName("button-layout");
+
+        cardNumber.setValue(creditCard.getNumCreditCard().toString());
+        cardNumber.setReadOnly(true);
+
+        cardholderName.setValue(creditCard.getPlaceholder().toString());
+        cardholderName.setReadOnly(true);
+
+        creditCardType.setValue(creditCard.getType().toString());
+        creditCardType.setReadOnly(true);
+
+        card_provider.setValue(creditCard.getCardProvider().toString());
+        card_provider.setReadOnly(true);
+
+        pin.setValue(creditCard.getPin().toString());
+        pin.setReadOnly(true);
+
+        cvv.setValue(creditCard.getCvv().toString());
+        cvv.setReadOnly(true);
+
+
+        dateField.setValue(creditCard.getExpirationDate().toString());
+        dateField.setReadOnly(true);
+
+        Salir.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        buttonLayout.add(Salir);
+        return buttonLayout;
+    }
+
+
+}
